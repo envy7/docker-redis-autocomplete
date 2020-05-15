f10048b49c43331bbea6ffe63238184db6781a53
Noel Varghese <noel.varghese.22@gmail.com>
2020-05-15 12:08:58 -0400

    Add return value bool for util functions

--- 3/app/flask/app/views.py
+++ 3/app/flask/app/views.py
@@ -1,25 +1,38 @@
 from app import app
+from flask import jsonify
 from app.utils import redis_healthcheck, redis_add_word, redis_autocomplete_word, validate_input
 
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    return redis_healthcheck()
+    response = redis_healthcheck()
+    if response:
+        return 'Healthy'
+    else:
+        return 'Unhealthy'
 
 
 @app.route('/add_word/<section>', methods=['GET'])
 def add_word(section):
-    if validate_input(section, "word"):
-        word = section.split("=")[1]
-        return redis_add_word(word)
+    if validate_input(section, 'word'):
+        word = section.split('=')[1]
+        response = redis_add_word(word)
+        if response:
+            return(jsonify({'success': 'word inserted to dictionary'})), 200
+        else:
+            return 'Word was not added successfully, please try again', 503
     else:
-        return "API Usage is /add_word/word=<your word>"
+        return(jsonify({'error': 'API Usage is /add_word/word=<your word>'})), 400
 
 
 @app.route('/autocomplete/<section>', methods=['GET'])
 def autocomplete(section):
-    if validate_input(section, "query"):
-        query = section.split("=")[1]
-        return redis_autocomplete_word(query)
+    if validate_input(section, 'query'):
+        query = section.split('=')[1]
+        response = redis_autocomplete_word(query)
+        if response:
+            return response, 200
+        else:
+            return 'Unable to autocomplete, please try again', 503
     else:
-        return "API Usage is /autocomplete/query=<your word>"
+        return(jsonify({'error': 'API Usage is /autocomplete/query=<your word>'})), 400
