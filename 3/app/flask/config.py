5b0555979c1c3975acb02df1ca74647c7c5bbda8
Noel Varghese <noel.varghese.22@gmail.com>
2020-05-15 07:46:57 -0400

    Changing default config values and environment variables

--- 3/app/flask/config.py
+++ 3/app/flask/config.py
@@ -6,7 +6,7 @@ class Config:
     """Set configuration vars from env variables"""
 
     # Redis
-    REDIS_HOST = environ.get('REDIS_HOST', default="redis")
+    REDIS_HOST = environ.get('REDIS_HOST', default="localhost")
     REDIS_PORT = environ.get('REDIS_PORT', default=6379)
     REDIS_DATABASE = environ.get('REDIS_DATABASE', default=0)
-    REDIS_ZSET = environ.get('REDIS_ZSET', default="autocomplete")
+    REDIS_ZSET = environ.get('REDIS_ZSET', default="zset")
