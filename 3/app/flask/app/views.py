63e4997d356d858d10c1944d78cdff7e9b1682d0
Noel Varghese <noel.varghese.22@gmail.com>
2020-05-15 23:34:13 -0400

    Revert older change, health status should return 503 if unhealthy

--- 3/app/flask/app/views.py
+++ 3/app/flask/app/views.py
@@ -14,7 +14,7 @@ def health_check():
     if response:
         return 'Healthy', 200
     else:
-        return 'Unhealthy', 200
+        return 'Unhealthy', 503
 
 
 @app.route('/add_word/<section>', methods=['POST'])
